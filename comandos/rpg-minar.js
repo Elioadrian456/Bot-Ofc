856ec84e0e0a2f529cbd3eefbca9d311b6a040a3
Elioadrian456 <125603977+Elioadrian456@users.noreply.github.com>
2023-02-17 11:58:21 -0300

    Add files via upload

---
+++ comandos/rpg-minar.js
@@ -0,0 +1,29 @@
+let handler = async (m, { conn, isPrems}) => {
+let hasil = Math.floor(Math.random() * 1000)
+let time = global.db.data.users[m.sender].lastmiming + 600000
+if (new Date - global.db.data.users[m.sender].lastmiming < 600000) throw `*⏰ Espera ${msToTime(time - new Date())} Para volver a minar*`  
+let info = `
+*[ ⛏️ ] Genial minaste ${hasil} XP*`.trim()
+global.db.data.users[m.sender].lastmiming = new Date * 1
+
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🔥 THE DORRAT - BOT 🔥*', 'status@broadcast')
+}
+handler.help = ['minar']
+handler.tags = ['xp']
+handler.command = ['minar', 'miming', 'mine'] 
+handler.fail = null
+handler.exp = 0
+export default handler
+
+function msToTime(duration) {
+var milliseconds = parseInt((duration % 1000) / 100),
+seconds = Math.floor((duration / 1000) % 60),
+minutes = Math.floor((duration / (1000 * 60)) % 60),
+hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+
+hours = (hours < 10) ? "0" + hours : hours
+minutes = (minutes < 10) ? "0" + minutes : minutes
+seconds = (seconds < 10) ? "0" + seconds : seconds
+
+return minutes + " m y " + seconds + " s " 
+}
